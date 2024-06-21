c3d5bd347ab15b06148b951bf126d90d7c9c0636
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-21 13:12:18 +0530

    Customized CSS for the header

--- App.js
+++ App.js
@@ -4,14 +4,14 @@ import ReactDOM from "react-dom/client";
 const Header = () => (
     <div className="header" >
         <div className="logo-container">
-            <img src="https://imgs.search.brave.com/l0684uJjP2-PEeXdGv36QCfwESGiaMssVCaeHKdgqTc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zZWVr/bG9nby5jb20vaW1h/Z2VzL1QvVGFjb19C/ZWxsLWxvZ28tN0Uy/MEMzMzU0RS1zZWVr/bG9nby5jb20ucG5n"/>
+            <img  className="logo" src="https://imgs.search.brave.com/l0684uJjP2-PEeXdGv36QCfwESGiaMssVCaeHKdgqTc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zZWVr/bG9nby5jb20vaW1h/Z2VzL1QvVGFjb19C/ZWxsLWxvZ28tN0Uy/MEMzMzU0RS1zZWVr/bG9nby5jb20ucG5n"/>
         </div>
         <div className="nav-items" >
             <ul>
                 <li>Home</li>
                 <li>About us</li>
-                <li>Cart</li>
                 <li>Help</li>
+                <li>Cart</li>
             </ul>
         </div>
     </div>
