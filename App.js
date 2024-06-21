15be75873e79e617a20e96ef9a13ce7dfe408c6d
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-21 12:39:37 +0530

    Added separate divs for body, header, footer and made a high level AppLayout component

--- App.js
+++ App.js
@@ -7,6 +7,27 @@ const Body = () => (
     </div>
 )
 
+const Header = () => (
+    <div>
+        <h1>Welcome to the food app</h1>
+    </div>
+)
+
+const Footer = () => (
+    <div>
+        <h1>Welcome to the food app</h1>
+    </div>
+)
+
+const AppLayout = () => (
+    <>
+        <Header/>
+        <Body/>
+        <Footer/>
+    </>
+)
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Body></Body>);
+root.render(<AppLayout/>);
 
