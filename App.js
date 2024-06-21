0f9193242d8ccd75172bce57900b4b78c32d06f6
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-21 12:27:00 +0530

    Added reacy body outline component

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const body = () => (
+    <div>
+        <h1>Welcome to the food app</h1>
+    </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(body);
+
