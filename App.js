a8ca3e71029e0a58a082337b64774e1bad9148c3
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-21 15:37:56 +0530

    Modified resto card to contain cock ra ci

--- App.js
+++ App.js
@@ -20,13 +20,13 @@ const Header = () => (
 
 const RestoCard = () => (
     <div className="res-card">
-        <h3 className="res-name" >Name</h3>
-        <img className="res-img" src="https://imgs.search.brave.com/l0684uJjP2-PEeXdGv36QCfwESGiaMssVCaeHKdgqTc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zZWVr/bG9nby5jb20vaW1h/Z2VzL1QvVGFjb19C/ZWxsLWxvZ28tN0Uy/MEMzMzU0RS1zZWVr/bG9nby5jb20ucG5n" />
-        <h3>Rating</h3>
-        <h3>Location</h3>
-        <text>Cuisine</text>
-        <text>Distance</text>
-        <text>Time</text>
+        <h3 className="res-name" >Cock Ra Co</h3>
+        <img className="res-img" src="https://imgs.search.brave.com/6S2dgLZXG22sVdx4GqqT50BrYjXr6NmEUWCDHBwNMGk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuamRtYWdpY2Jv/eC5jb20vY29tcC9j/b2ltYmF0b3JlL3M1/LzA0MjJweDQyMi54/NDIyLjExMDEwMjEy/MDEzOS55OXM1L2Nh/dGFsb2d1ZS9jb2Nr/LXJhLWNvLXJzLXB1/cmFtLWNvaW1iYXRv/cmUtY29pbWJhdG9y/ZS1ob21lLWRlbGl2/ZXJ5LXJlc3RhdXJh/bnRzLW02dTRjLmpw/Zz9jbHI9IzFjNGEx/Yw" />
+        <h3>Rating: 5</h3>
+        <h3>RS Puram</h3>
+        <text>South Indian</text>
+        <text>20 Km</text>
+        <text>45 mins</text>
     </div>
 )
 
@@ -36,6 +36,8 @@ const Body = () => (
             Search
         </div>
         <div className="res-container">
+            <RestoCard />
+            <RestoCard />
             <RestoCard />
             <RestoCard />
             <RestoCard />
