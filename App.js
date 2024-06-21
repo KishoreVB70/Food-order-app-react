7a10f93298a1daed6ba531bda1e4335ad1f771db
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-21 12:55:49 +0530

    Populated the Header

--- App.js
+++ App.js
@@ -2,26 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Header = () => (
-    <div id="header" >
-        <h1>Welcome to the food app</h1>
+    <div className="header" >
+        <div className="logo-container">
+            <img src="https://imgs.search.brave.com/l0684uJjP2-PEeXdGv36QCfwESGiaMssVCaeHKdgqTc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zZWVr/bG9nby5jb20vaW1h/Z2VzL1QvVGFjb19C/ZWxsLWxvZ28tN0Uy/MEMzMzU0RS1zZWVr/bG9nby5jb20ucG5n"/>
+        </div>
+        <div className="nav-items" >
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Cart</li>
+                <li>Help</li>
+            </ul>
+        </div>
     </div>
 )
 
 const Body = () => (
-    <div id="body" >
+    <div className="body" >
         <h1>Welcome to the food app</h1>
     </div>
 )
 
 
 const Footer = () => (
-    <div id="footer" >
+    <div className="footer" >
         <h1>Welcome to the food app</h1>
     </div>
 )
 
 const AppLayout = () => (
-    <div id="App">
+    <div className="App">
         <Header/>
         <Body/>
         <Footer/>
