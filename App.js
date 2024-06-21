2a407d4c37f6962b5c2a793f40ab9b9d9fa24228
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-21 16:24:31 +0530

    Packaged the rest details into resdata array of objects

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const restData = [
+    {resName: "Cock ra co", cuisine: "South Indian", rating: 5},
+    {resName: "La Cafe", cuisine: "Veg", rating: 5},
+    {resName: "French Door", cuisine: "Continental", rating: 4}
+]
 const Header = () => (
     <div className="header" >
         <div className="logo-container">
@@ -20,11 +25,11 @@ const Header = () => (
 
 const RestoCard = (props) => (
     <div className="res-card">
-        <h3 className="res-name" >{props.resName}</h3>
+        <h3 className="res-name" >{props.data.resName}</h3>
         <img className="res-img" src="https://imgs.search.brave.com/6S2dgLZXG22sVdx4GqqT50BrYjXr6NmEUWCDHBwNMGk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuamRtYWdpY2Jv/eC5jb20vY29tcC9j/b2ltYmF0b3JlL3M1/LzA0MjJweDQyMi54/NDIyLjExMDEwMjEy/MDEzOS55OXM1L2Nh/dGFsb2d1ZS9jb2Nr/LXJhLWNvLXJzLXB1/cmFtLWNvaW1iYXRv/cmUtY29pbWJhdG9y/ZS1ob21lLWRlbGl2/ZXJ5LXJlc3RhdXJh/bnRzLW02dTRjLmpw/Zz9jbHI9IzFjNGEx/Yw" />
-        <h4>Rating: 5</h4>  
+        <h4>Rating: {props.data.rating}</h4>  
         <h4>RS Puram</h4>
-        <text>South Indian</text>
+        <text>{props.data.cuisine}</text>
         <text>20 Km</text>
         <text>45 mins</text>
     </div>
@@ -36,9 +41,9 @@ const Body = () => (
             Search
         </div>
         <div className="res-container">
-            <RestoCard resName="Cock Ra Co"/>
-            <RestoCard resName="La cafe"/>
-            <RestoCard resName="French Door"/>
+            <RestoCard data={restData[0]}/>
+            <RestoCard data={restData[1]}/>
+            <RestoCard data={restData[2]}/>
         </div>
     </div>
 )
