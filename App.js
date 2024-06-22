b592e04f7aa192444a410386ddb3cc1440b0e5f6
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-22 15:28:39 +0530

    Minor UI changes

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const restData = [
-    {resName: "Cock ra co", cuisine: "South Indian", rating: 5},
-    {resName: "La Cafe", cuisine: "Veg", rating: 5},
-    {resName: "French Door", cuisine: "Continental", rating: 4}
+    {resName: "Cock ra co", cuisine: "South Indian", rating: 5, costForTwo: 400, imageUrl: "https://imgs.search.brave.com/6S2dgLZXG22sVdx4GqqT50BrYjXr6NmEUWCDHBwNMGk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuamRtYWdpY2Jv/eC5jb20vY29tcC9j/b2ltYmF0b3JlL3M1/LzA0MjJweDQyMi54/NDIyLjExMDEwMjEy/MDEzOS55OXM1L2Nh/dGFsb2d1ZS9jb2Nr/LXJhLWNvLXJzLXB1/cmFtLWNvaW1iYXRv/cmUtY29pbWJhdG9y/ZS1ob21lLWRlbGl2/ZXJ5LXJlc3RhdXJh/bnRzLW02dTRjLmpw/Zz9jbHI9IzFjNGEx/Yw"},
+    {resName: "La Cafe", cuisine: "Veg", rating: 5,costForTwo: 350, imageUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/bzboqpm6chvobfxcfyem"},
+    {resName: "French Door", cuisine: "Continental", rating: 4, costForTwo: 1000, imageUrl: "https://imgs.search.brave.com/LOIN2JXNMFgDORdzTgLz1ZhYNQlsTwQBB0ueviJ_KeE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS1jZG4udHJpcGFk/dmlzb3IuY29tL21l/ZGlhL3Bob3RvLW8v/MGYvMjcvMTMvYjkv/dGhlLWZyZW5jaC1k/b29yLmpwZw"}
 ]
 const Header = () => (
     <div className="header" >
@@ -23,15 +23,16 @@ const Header = () => (
 )
 
 
-const RestoCard = (props) => (
+const RestoCard = ({data}) => (
     <div className="res-card">
-        <h3 className="res-name" >{props.data.resName}</h3>
-        <img className="res-img" src="https://imgs.search.brave.com/6S2dgLZXG22sVdx4GqqT50BrYjXr6NmEUWCDHBwNMGk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuamRtYWdpY2Jv/eC5jb20vY29tcC9j/b2ltYmF0b3JlL3M1/LzA0MjJweDQyMi54/NDIyLjExMDEwMjEy/MDEzOS55OXM1L2Nh/dGFsb2d1ZS9jb2Nr/LXJhLWNvLXJzLXB1/cmFtLWNvaW1iYXRv/cmUtY29pbWJhdG9y/ZS1ob21lLWRlbGl2/ZXJ5LXJlc3RhdXJh/bnRzLW02dTRjLmpw/Zz9jbHI9IzFjNGEx/Yw" />
-        <h4>Rating: {props.data.rating}</h4>  
+        <h3 className="res-name" >{data.resName}</h3>
+        <img className="res-img" src={data.imageUrl} />
+        <h4>Rating: {data.rating}</h4>  
         <h4>RS Puram</h4>
-        <text>{props.data.cuisine}</text>
+        <text>{data.cuisine}</text>
         <text>20 Km</text>
         <text>45 mins</text>
+        <text>Cost for two: {data.costForTwo}</text>
     </div>
 )
 
