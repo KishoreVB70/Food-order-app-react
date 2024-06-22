7d050da3839fa5f262a5326fc1d610d6134f2006
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-22 21:33:24 +0530

    Replaced the static data with dynamic prop data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-const Header = () => (
+const Header = ({appLogo}) => (
     <div className="header" >
         <div className="logo-container">
-            <img  className="logo" src="https://imgs.search.brave.com/l0684uJjP2-PEeXdGv36QCfwESGiaMssVCaeHKdgqTc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zZWVr/bG9nby5jb20vaW1h/Z2VzL1QvVGFjb19C/ZWxsLWxvZ28tN0Uy/MEMzMzU0RS1zZWVr/bG9nby5jb20ucG5n"/>
+            <img  className="logo" src={appLogo} />
         </div>
         <div className="nav-items" >
             <ul>
