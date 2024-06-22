671a87239e6c31518cb0f4cb61c297de91b843e6
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-22 16:02:56 +0530

    Added unique key for the data and used p tag instead of text tag

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const restData = [
-    {resName: "Cock ra co", cuisine: "South Indian", rating: 5, costForTwo: 400, imageUrl: "https://imgs.search.brave.com/6S2dgLZXG22sVdx4GqqT50BrYjXr6NmEUWCDHBwNMGk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuamRtYWdpY2Jv/eC5jb20vY29tcC9j/b2ltYmF0b3JlL3M1/LzA0MjJweDQyMi54/NDIyLjExMDEwMjEy/MDEzOS55OXM1L2Nh/dGFsb2d1ZS9jb2Nr/LXJhLWNvLXJzLXB1/cmFtLWNvaW1iYXRv/cmUtY29pbWJhdG9y/ZS1ob21lLWRlbGl2/ZXJ5LXJlc3RhdXJh/bnRzLW02dTRjLmpw/Zz9jbHI9IzFjNGEx/Yw"},
-    {resName: "La Cafe", cuisine: "Veg", rating: 5,costForTwo: 350, imageUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/bzboqpm6chvobfxcfyem"},
-    {resName: "French Door", cuisine: "Continental", rating: 4, costForTwo: 1000, imageUrl: "https://imgs.search.brave.com/LOIN2JXNMFgDORdzTgLz1ZhYNQlsTwQBB0ueviJ_KeE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS1jZG4udHJpcGFk/dmlzb3IuY29tL21l/ZGlhL3Bob3RvLW8v/MGYvMjcvMTMvYjkv/dGhlLWZyZW5jaC1k/b29yLmpwZw"}
+    {resId: 0, resName: "Cock ra co", cuisine: "South Indian", rating: 5, costForTwo: 400, imageUrl: "https://imgs.search.brave.com/6S2dgLZXG22sVdx4GqqT50BrYjXr6NmEUWCDHBwNMGk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuamRtYWdpY2Jv/eC5jb20vY29tcC9j/b2ltYmF0b3JlL3M1/LzA0MjJweDQyMi54/NDIyLjExMDEwMjEy/MDEzOS55OXM1L2Nh/dGFsb2d1ZS9jb2Nr/LXJhLWNvLXJzLXB1/cmFtLWNvaW1iYXRv/cmUtY29pbWJhdG9y/ZS1ob21lLWRlbGl2/ZXJ5LXJlc3RhdXJh/bnRzLW02dTRjLmpw/Zz9jbHI9IzFjNGEx/Yw"},
+    {resId: 1, resName: "La Cafe", cuisine: "Veg", rating: 5,costForTwo: 350, imageUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/bzboqpm6chvobfxcfyem"},
+    {resId: 2, resName: "French Door", cuisine: "Continental", rating: 4, costForTwo: 1000, imageUrl: "https://imgs.search.brave.com/LOIN2JXNMFgDORdzTgLz1ZhYNQlsTwQBB0ueviJ_KeE/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS1jZG4udHJpcGFk/dmlzb3IuY29tL21l/ZGlhL3Bob3RvLW8v/MGYvMjcvMTMvYjkv/dGhlLWZyZW5jaC1k/b29yLmpwZw"}
 ]
 const Header = () => (
     <div className="header" >
@@ -31,10 +31,10 @@ const RestoCard = ({data}) =>  {
             <img className="res-img" src={imageUrl} />
             <h4>Rating: {rating}</h4>  
             <h4>RS Puram</h4>
-            <text>{cuisine}</text>
-            <text>20 Km</text>
-            <text>45 mins</text>
-            <text>Cost for two: {costForTwo}</text>
+            <p>{cuisine}</p>
+            <p>20 Km</p>
+            <p>45 mins</p>
+            <p>Cost for two: {costForTwo}</p>
         </div>
     )
 }
@@ -46,7 +46,7 @@ const Body = () => (
         </div>
         <div className="res-container">
             {
-                restData.map(rest => (<RestoCard data={rest} />))
+                restData.map(rest => (<RestoCard key={rest.resId} data={rest} />))
             }
         </div>
     </div>
