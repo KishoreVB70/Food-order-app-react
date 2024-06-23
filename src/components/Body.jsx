79149ea600188c3b9f09ea0fff9169f9d49b09c3
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-23 08:01:40 +0530

    Added click event handler to the filter button

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,13 @@
 import ResCard from "./ResCard";
 
+const bot = () => console.log("bot");
+
 const Body = ( {resData} ) => (
     <div className="body" >
         <div className="search">
             Search
         </div>
+        <button id="top-rated" onClick={bot}>Top Rated restaurants</button>
         <div className="res-container">
             {
                 resData.map(rest => (<ResCard key={rest.resId} data={rest} />))
