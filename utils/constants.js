ade91d7be0914b1555c51aeadb7af4c1f1dceb9b
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-23 20:10:19 +0530

    Added swiggy API to constants.js

--- utils/constants.js
+++ utils/constants.js
@@ -6,4 +6,6 @@ const resData = [
 
 export const appLogo = "https://imgs.search.brave.com/l0684uJjP2-PEeXdGv36QCfwESGiaMssVCaeHKdgqTc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zZWVr/bG9nby5jb20vaW1h/Z2VzL1QvVGFjb19C/ZWxsLWxvZ28tN0Uy/MEMzMzU0RS1zZWVr/bG9nby5jb20ucG5n";
 
+export const swiggyAPI = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.0168445&lng=76.9558321&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
 export default resData;
