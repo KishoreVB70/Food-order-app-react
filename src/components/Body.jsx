753e1f02e890a7410596938d7e50c9c3856867a0
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-23 16:31:03 +0530

    Used useState Hook to set the dynamic filter functionality

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,27 @@
 import ResCard from "./ResCard";
+import { useState } from "react";
 
-const bot = () => console.log("bot");
 
-const Body = ( {resData} ) => (
-    <div className="body" >
-        <div className="search">
-            Search
+
+const Body = ( {resData} ) => {
+    const [resData, setResData] = useState(resData);
+    const bot = () => {
+        setResData( (data) => resData.filter(res => res.rating > 4.2));
+        console.log(resData)
+    } 
+
+    return (
+        <div className="body" >
+            <div className="filter">
+                <button className="rated-btn" onClick={bot}>Top Rated restaurants</button>
+            </div>
+            <div className="res-container">
+                {
+                    resData.map(rest => (<ResCard key={rest.resId} data={rest} />))
+                }
+            </div>
         </div>
-        <button id="top-rated" onClick={bot}>Top Rated restaurants</button>
-        <div className="res-container">
-            {
-                resData.map(rest => (<ResCard key={rest.resId} data={rest} />))
-            }
-        </div>
-    </div>
-)
+    )
+}
 
 export default Body;
