3d8a189c38a7852373f5a5ea95ae3b00250817e4
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-23 21:06:12 +0530

    Added dynamic login, logout button using useState Hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,35 @@
-const Header = ({appLogo}) => (
-    <div className="header" >
-        <div className="logo-container">
-            <img  className="logo" src={appLogo} />
+import { useState } from "react";
+
+const Header = ({appLogo}) => { 
+
+    //State variable
+    const [login, setLogin] = useState("Login");
+
+    const handleLogin = () => {
+        setLogin((data) => {
+            if(data === "Login") {
+                return "Logout"
+            }
+            return "Login"
+        })
+    }
+
+    return (
+        <div className="header" >
+            <div className="logo-container">
+                <img  className="logo" src={appLogo} />
+            </div>
+            <div className="nav-items" >
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Help</li>
+                    <li>Cart</li>
+                    <button onClick={handleLogin} className="login-btn" >{login}</button>
+                </ul>
+            </div>
         </div>
-        <div className="nav-items" >
-            <ul>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Help</li>
-                <li>Cart</li>
-            </ul>
-        </div>
-    </div>
-)
+    ) 
+}
 
 export default Header;
