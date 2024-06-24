7cf543b157271bb5b7d4cb228f9b25646d79147c
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-24 12:53:54 +0530

    Added Link to contact us

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => (
     <div className="footer" >
         <ul>
             <li>Copyright</li>
             <li>Socials</li>
             <li>Address</li>
-            <li>Contact</li>
+            <li>
+                <Link to={"/contact"} >Contact Us</Link>
+                
+            </li>
         </ul>
     </div>
 )
