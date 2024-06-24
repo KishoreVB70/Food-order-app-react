42e8d239a40dd45191abc3fd03ec172c09620f6d
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-24 20:31:18 +0530

    Made UI changes to render all the missing data by using ternary operators

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { swiggyCloudinaryImageBase } from '../../utils/constants';
 
 const MenuCard = ({data}) => {
-  const {name, ratings, imageId, defaultPrice} = data;
+  const {name, imageId, ratings, defaultPrice, price} = data;
+  const rating = ratings.aggregatedRating.rating
   return (
       <div className="res-card">
           <h3 className="res-name" >{name}</h3>
           <img className="res-img" src={swiggyCloudinaryImageBase + imageId} />
-          <h4>Rating:</h4>  
-          <p>{defaultPrice}</p>
+          <h4>Rating: {rating?rating:"No rating"}</h4>  
+          <p>Price: Rs.{defaultPrice?defaultPrice/100:price/100}</p>
       </div>
   )
 }
