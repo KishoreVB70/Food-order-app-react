e112262bbb733e6c4e60c5b55cb241e1ad60c87e
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-24 11:32:42 +0530

    created appRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router-dom";
 
 // Components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
+import AboutUs from "./components/AboutUs";
 
 // Utils
 import { appLogo } from "../utils/constants.js"
@@ -17,6 +19,18 @@ const AppLayout = () => (
         <Footer/>
     </div>
 )
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+    },
+    {
+        path: "/about",
+        element: <AboutUs />
+    }
+
+])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<AppLayout/>);
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,3 +5,4 @@ const AboutUs = () => {
         </div>
     )
 }
+export default AboutUs;
