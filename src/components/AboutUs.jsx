f72c3a697feb616f38033ec34d9c9cee6071294f
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-25 09:50:51 +0530

    converted about us into class based component

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,13 +1,17 @@
 import UserCard from "./UserCard";
 import UserClass from "./UserClass";
+import { Component } from "react";
 
-const AboutUs = () => {
-    return (
-        <div className="aboutus">
-            <h1>We are Swiggy</h1>
-            <h1>Here are our members:</h1>
-            <UserClass name="Class" />
-        </div>
-    )
+class AboutUs extends Component {
+    render() {
+        return (
+            <div className="aboutus">
+                <h1>We are Swiggy</h1>
+                <h1>Here are our members:</h1>
+                <UserClass name="Class" />
+            </div>
+        )
+    }
 }
+
 export default AboutUs;
