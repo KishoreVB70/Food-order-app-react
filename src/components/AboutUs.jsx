d6615bb45047bb81be7514b3ad6153792dc93368
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-25 08:34:14 +0530

    Added usercard component for the about us page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,7 +1,11 @@
+import UserCard from "./UserCard";
+
 const AboutUs = () => {
     return (
         <div className="aboutus">
             <h1>We are Swiggy</h1>
+            <h1>Here are our members:</h1>
+            <UserCard />
         </div>
     )
 }
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div className='user-card' >
+        <h1>Name</h1>
+        <h1>Location</h1>
+        <h1>Contact: Twitter</h1>
+    </div>
+  )
+}
+
+export default UserCard
