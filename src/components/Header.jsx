67a0eab948e6fee66e90e5db1c5ba12f8cc081be
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-25 15:42:37 +0530

    Added online status div for green and black

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../../hooks/useOnlineStatus";
 const Header = ({appLogo}) => { 
 
     //State variable
     const [login, setLogin] = useState("Login");
+    const [bg, setBg] = useState("green");
+
+    const onlineStatus = useOnlineStatus();
+
+    useEffect(() => {
+        onlineStatus===true?setBg("green"):setBg("red");
+    }, [onlineStatus])
 
     const handleLogin = () => {
         setLogin(data => data==="Login"?"Logout":"Login");
@@ -15,7 +23,7 @@ const Header = ({appLogo}) => {
                 <img  className="logo" src={appLogo} />
             </div>
             <div className="nav-items" >
-                <div className="online-status"></div>
+                <div className="online-status"style={{backgroundColor: bg}} ></div>
                 <ul>
                     <li>
                          <Link to={"/"}>Home</Link>
