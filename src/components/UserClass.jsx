1215c22e79c910ffd26f90f6be8e820d0c41dd45
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-25 09:38:03 +0530

    Added state variable to the class component

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
 class UserClass extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0,
+            bot: "Moto"
+        }
+    }
+
     render() {
+        const {count, bot} = this.state;
+        const {name} = this.props;
+
         return (
             <div className='user-card' >
-                <h1>Name: Show down {this.props.name} </h1>
-                <h1>Location: Indiana Pacers</h1>
-                <h1>Contact: Twitter</h1>
+                <h1>Name: Show down {name} </h1>
+                <h1>Location: Indiana Pacers {count}</h1>
+                <h1>Contact: Twitter {bot}</h1>
             </div>
         )
     }
