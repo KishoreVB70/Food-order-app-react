7019263d41e4060f72e3bac24de70305dbe14d5c
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-25 14:33:43 +0530

    Added try catch block for error handling

--- hooks/useRestaurant.js
+++ hooks/useRestaurant.js
@@ -8,32 +8,36 @@ const useRestaurant = () => {
     })
 
     const fetchRestaurant = async() => {
-        const data = await fetch(swiggyAPI);
-        const jsonData = await data.json();
-
-        // Optional chaining
-        const restaurants = 
-            jsonData.data?.cards[4]?.card?.card?.
-            gridElements?.infoWithStyle?.restaurants;
-        
-        const streamlinedRestaurants = restaurants.map((res) => {
-            const {
-                name, id, areaName, avgRating, 
-                cloudinaryImageId, costForTwo, cuisines
-            } = res.info;
-
-            return {
-                resId: id,
-                resName: name,
-                area: areaName,
-                rating: avgRating,
-                imageId: cloudinaryImageId,
-                costForTwo: costForTwo,
-                cuisine: cuisines[0]
-            };
-        });
-        
-        setResData(() => streamlinedRestaurants);
+        try {
+            const data = await fetch(swiggyAPI);
+            const jsonData = await data.json();
+    
+            // Optional chaining
+            const restaurants = 
+                jsonData.data?.cards[4]?.card?.card?.
+                gridElements?.infoWithStyle?.restaurants;
+            
+            const streamlinedRestaurants = restaurants.map((res) => {
+                const {
+                    name, id, areaName, avgRating, 
+                    cloudinaryImageId, costForTwo, cuisines
+                } = res.info;
+    
+                return {
+                    resId: id,
+                    resName: name,
+                    area: areaName,
+                    rating: avgRating,
+                    imageId: cloudinaryImageId,
+                    costForTwo: costForTwo,
+                    cuisine: cuisines[0]
+                };
+            });
+            
+            setResData(() => streamlinedRestaurants);
+        }catch(error) {
+            console.log(error);
+        }
     }
 
     return restData;
