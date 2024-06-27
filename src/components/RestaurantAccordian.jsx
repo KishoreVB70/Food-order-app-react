e91a00d5aa1d95b2c81961a66fbeeb6d321e3ae9
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-27 15:26:41 +0530

    Added add button

--- src/components/RestaurantAccordian.jsx
+++ src/components/RestaurantAccordian.jsx
@@ -4,7 +4,7 @@ import AccordianItem from './AccordianItem'
 function RestaurantAccordian({title, cards}) {
   return (
     <div className='w-6/12 flex flex-col items-center shadow-lg justify-center  pl-7 p-2 my-4 bg-gray-100 hover:p-3 hover:cursor-pointer hover:shadow-2xl' >
-        <div className='w-full text-center flex justify-between ' >
+        <div className='w-full text-center h-full flex justify-between ' >
             <h1 className=' font-bold ' >{title} ({cards.length})</h1>
             <span className="text-lg">⏬</span>
         </div>
