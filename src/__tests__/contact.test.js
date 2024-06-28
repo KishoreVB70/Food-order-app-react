508c9a9d324068f5afae14577732fdec9bd97e46
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-28 16:40:10 +0530

    Added test for contact

---
+++ src/__tests__/contact.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react"
+import Contact from "../components/Contact"
+
+
+test("Is component loading", () => {
+    render(<Contact />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+})
