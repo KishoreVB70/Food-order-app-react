feff991fef92a6c1716c8743335cee6dda471580
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-28 16:27:24 +0530

    populated the Contact component for testing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div><h1>Swiggy@gmail.com</h1></div>
+    <div className='border border-gray-500 flex flex-col justify-center items-center p-2 w-6/12 my-4 mx-auto' >
+      <h1>Swiggy@gmail.com</h1>
+      <input  className='border m-1 p-1 text-center border-gray-500 ' type="text" placeholder='name' />
+      <input  className='border m-1 p-1 text-center border-gray-500 ' type="text" placeholder='contact' />
+      <input  className='border m-1 p-1 text-center border-gray-500 ' type="text" placeholder='issue' />
+      <button className='m-2 p-2 border rounded-lg text-white bg-blue-500 hover:bg-blue-100 hover:text-blue-700' >Submit</button>
+    </div>
   )
 }
 
