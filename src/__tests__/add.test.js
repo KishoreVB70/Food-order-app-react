5ccfc786eb3a1fb3dc0cada7ee16082cafae57a6
KishoreVB70 <kishore07kzr@gmail.com>
2024-06-28 15:49:57 +0530

    Another way to write test using distinct function rather than inline arrow functions

--- src/__tests__/add.test.js
+++ src/__tests__/add.test.js
@@ -1,7 +1,8 @@
-import { hasUncaughtExceptionCaptureCallback } from "process";
 import add from "../add";
 
-test('should return the sum of two numbers', () => {
+function bot() {
     const ans = add(1,2);
     expect(ans).toBe(3);
- })
+}
+
+test('should return the sum of two numbers', bot);
