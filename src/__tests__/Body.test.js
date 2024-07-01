4858f463c0f35b5400be60fc9f0150f84a675092
KishoreVB70 <kishore07kzr@gmail.com>
2024-07-01 12:49:36 +0530

    added jest watch

--- src/__tests__/Body.test.js
+++ src/__tests__/Body.test.js
@@ -2,6 +2,8 @@ import { render, screen } from "@testing-library/react"
 import Body from "../components/Body"
 import "@testing-library/jest-dom";
 import data from "../mocks/mockJsonDataRestaurants.json";
+import { BrowserRouter } from "react-router-dom";
+import { act } from "react";
 
 global.fetch = jest.fn(() => {
     return Promise.resolve({
@@ -9,8 +11,15 @@ global.fetch = jest.fn(() => {
     })
 })
 
-it("Should render with search", () => {
-    render(<Body />);
+it("Should render with search", async() => {
+    await act(async() => {
+        render(
+            <BrowserRouter>
+                <Body />
+            </BrowserRouter>
+        )
+    }
+)
     const searchBox = screen.getByPlaceholderText("search");
     expect(searchBox).toBeInTheDocument();
 })
